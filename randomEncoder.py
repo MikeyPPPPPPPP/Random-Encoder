58ac6c8c7a4505e74d2c419b147795bd9724f706
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2021-11-29 19:12:42 -0800

    optimized
    Made a function a tuple comprehension.

--- randomEncoder.py
+++ randomEncoder.py
@@ -7,9 +7,8 @@ class encoderGenrator:
 		self.text = text
 		self.code = {}
 		self.alpha = ['\t','\n',' ','!', '~', '`', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', '-', '+', '{', '}', '|', '\\', '}', '{', '[', ']', ':', ';', '"', "'", ',', '.', '/', '?', '>', '<', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'u', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'U', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-		self.encodeedText = ''
 		self.genrateKeys()
-		self.bEncode()
+		self.encodeedText = (base64.b64encode(''.join(self.disHolder).encode('ascii'))).decode('ascii')
 
 	def genrateKeys(self):
 		for x in self.alpha:
@@ -20,14 +19,6 @@ class encoderGenrator:
 					break
 				else:
 					self.randomCherector = self.alpha[random.randint(0,len(self.alpha)-1)]
-
-	def bEncode(self):
-		self.disHolder = []
-		for x in self.text:
-			self.disHolder.append(self.code[x])
-		self.encodeedText = (base64.b64encode(''.join(self.disHolder).encode('ascii'))).decode('ascii')
-		#return (base64.b64encode(''.join(self.disHolder).encode('ascii'))).decode('ascii')
-
 
 	def codeGenrator(self):
 		return('import base64;exec(\'\'.join([y[0] for x in [x for x in base64.b64decode( (\''+self.encodeedText+'\').encode(\'ascii\') ).decode(\'ascii\')] for y in [[x[0], x[1]] for x in '+str(self.code)+'.items()] if x == y[1]]))')
