925a2f8b3d8e6d00ca3c990277f62b6c75a61e5d
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2021-12-08 20:34:54 -0800

    Update randomEncoder.py

--- randomEncoder.py
+++ randomEncoder.py
@@ -21,7 +21,7 @@ class encoderGenrator:
 					self.randomCherector = self.alpha[random.randint(0,len(self.alpha)-1)]
 
 	def codeGenrator(self):
-		return('import base64;exec(\'\'.join([y[0] for x in [x for x in base64.b64decode( (\''+self.encodeedText+'\').encode(\'ascii\') ).decode(\'ascii\')] for y in [[x[0], x[1]] for x in '+str(self.code)+'.items()] if x == y[1]]))')
+		return 'import base64;exec(\'\'.join([y[0] for x in [x for x in base64.b64decode( (\''+self.encodeedText+'\').encode(\'ascii\') ).decode(\'ascii\')] for y in [[x[0], x[1]] for x in '+str(self.code)+'.items()] if x == y[1]]))'
 
 
 newFile = open('conMan.py','w')
